a3c28f5d3b09d36e3187ed7971d607bec974255e
Beto Esquivel <ppbeto94@gmail.com>
2014-10-31 13:33:12 -0600

    Finished second problem

--- 1139626_1195815_Tarea6.hs
+++ 1139626_1195815_Tarea6.hs
@@ -1,1 +1,10 @@
 -- Haskell homework for our Programming Languages Class
+
+--Implementar la función recursiva multiplica que obtenga una lista de 1’s que 
+--represente el resultado en unario de multiplicar dos enteros no negativos en decimal. 
+multiplica :: Integer -> Integer -> [Integer] 
+multiplica a b = repite (a * b)
+
+repite :: Integer -> [Integer]
+repite 0 = []
+repite n = [1] ++ repite (n - 1)
