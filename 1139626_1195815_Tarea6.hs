2132c7533eac0247ddae87e4ff8bdfa5f2b43e14
Beto Esquivel <ppbeto94@gmail.com>
2014-10-31 14:15:37 -0600

    Problema 1 de estructura de datos terminado.

--- 1139626_1195815_Tarea6.hs
+++ 1139626_1195815_Tarea6.hs
@@ -1,6 +1,6 @@
 -- Haskell homework for our Programming Languages Class
 
---Implementar la función recursiva multiplica que obtenga una lista de 1’s que 
+--2. Implementar la función recursiva multiplica que obtenga una lista de 1’s que 
 --represente el resultado en unario de multiplicar dos enteros no negativos en decimal. 
 multiplica :: Integer -> Integer -> [Integer] 
 multiplica a b = repite (a * b)
@@ -8,3 +8,25 @@ multiplica a b = repite (a * b)
 repite :: Integer -> [Integer]
 repite 0 = []
 repite n = [1] ++ repite (n - 1)
+
+--4. Implementar la función obten-mayores en Scheme que dados un árbol binario y
+--un valor como argumentos, cree una lista con los valores de los nodos que contengan
+--valores mayores que el valor dado como argumento. Los valores en la lista resultante
+--pueden, o no, estar ordenados
+data AB t = A (AB t) t (AB t) | V deriving Show
+ab = A (A (A V 2 V) 
+          5 
+          (A V 7 V)) 
+            8 
+          (A V 
+            9 
+            (A (A V 11 V) 
+            15 
+            V))
+
+
+obtenMayores :: AB Integer -> Integer -> [Integer] 
+obtenMayores V valor = []
+obtenMayores (A l v r)  valor   
+    | v > valor = [v] ++ (obtenMayores l valor) ++ (obtenMayores r valor)
+    | otherwise = (obtenMayores r valor)
